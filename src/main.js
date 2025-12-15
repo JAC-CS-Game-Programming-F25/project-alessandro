3a2ae5542f27e515de78e0488a67f8e4a6b2241d
aleburrascano <aleburrascano123@gmail.com>
2025-12-14 22:05:09 -0500

    Added credits in main.js to authors of game assets, both spritesheets and sounds

--- src/main.js
+++ src/main.js
@@ -1,3 +1,23 @@
+/**
+ * Stealth Heist
+ *
+ * ASSET CREDITS:
+ *
+ * SPRITES:
+ * - Characters and Maps: Modern Interiors by LimeZu (https://limezu.itch.io/moderninteriors)
+ *
+ * SOUNDS:
+ * - Footsteps, Whoosh, Navigate, Select, Exit: 400 Sounds Pack by Chequered Ink (https://ci.itch.io/400-sounds-pack)
+ * - Money Pickup (Cha-Ching): https://www.youtube.com/watch?v=tqNP8SiB90s
+ * - Alert: https://www.youtube.com/watch?v=_8lkmC0jGug
+ * - Game Over: https://www.youtube.com/watch?v=nPGvhgOMu9g
+ * - Pause: https://www.youtube.com/watch?v=C30KnGCiD80
+ * - Victory: https://www.youtube.com/watch?v=VJ8FQSh-H4U
+ * - Clock Tick: https://www.youtube.com/watch?v=GMUhTsAsOTI
+ * - Main Menu Music: https://www.youtube.com/watch?v=JiAlZIW7hsQ
+ * - In-Game Music: https://www.youtube.com/watch?v=70ASH5TPwzo
+ */
+
 import GameStateName from "./enums/GameStateName.js";
 import Game from "../lib/Game.js";
 import {
